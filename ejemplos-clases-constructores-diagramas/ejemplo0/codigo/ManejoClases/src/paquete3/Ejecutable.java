edc7b40e28882e7095506b0a967b191cd6f99f08
Kevin Robles <porkevin743@gmail.com>
2024-05-01 16:33:12 -0500

    Cambios

---
+++ ejemplos-clases-constructores-diagramas/ejemplo0/codigo/ManejoClases/src/paquete3/Ejecutable.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete3;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejecutable {
+
+    public static void main(String[] args) {
+        // En esta clase principal del metodo main donde creamos un objeto de 
+        // tipo Computadora no pedimos parametros ya que estamos usando el
+        // constructor que viene por defecto.
+        Computadora portatil = new Computadora();
+
+    }
+}
