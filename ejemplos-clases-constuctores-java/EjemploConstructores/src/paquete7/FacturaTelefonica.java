73129189956b42555f7dbe033d14fce3ae805c48
Kevin Robles <porkevin743@gmail.com>
2024-05-02 16:13:07 -0500

    Cambios

--- ejemplos-clases-constuctores-java/EjemploConstructores/src/paquete7/FacturaTelefonica.java
+++ ejemplos-clases-constuctores-java/EjemploConstructores/src/paquete7/FacturaTelefonica.java
@@ -60,5 +60,18 @@ public class FacturaTelefonica {
     public double obtenerValorFactura() {
         return valorFactura;
     }
+    @Override
+    public String toString(){
+        String cadena;
+        cadena= String.format("Objeto sin argumentos\n"
+                + "Número telefónico: %s\n"
+                + "Minutos mes: %.2f\nValor minuto: %.2f\n"
+                + "Valor factura: %.2f\n\n", obtenerNumeroTelefono(),
+                obtenerMinutosMes(),
+                obtenerValorMinuto(),
+                obtenerValorFactura());
+        return cadena;
+        
+    }
 
 }
